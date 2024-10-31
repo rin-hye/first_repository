c55be56d9527330e7be7707ac0ab0c69ab307584
rin-hye <rinsu0906@gmail.com>
2024-10-31 09:45:45 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,7 @@
 def print_hello():
-    print("Hello World!!")
+    print("Not Hello World!!")
+    print("Not Hello World!!")
+    print("Not Hello World!!")
 
 if __name__ == '__main__':
     print_hello()
