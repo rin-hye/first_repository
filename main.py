730e43e518bca507f8cd76a8cd251b97046d8281
rin-hye <rinsu0906@gmail.com>
2024-10-31 09:57:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import prime_number as pn
 
 if __name__ == '__main__':
     pn.is_prime_number(1)
-    pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(31)
-    pn.is_prime_number(45)
-    pn.is_prime_number(97)
+    pn.is_prime_number(7487)
+    pn.is_prime_number(784768)
+    pn.is_prime_number(687)
